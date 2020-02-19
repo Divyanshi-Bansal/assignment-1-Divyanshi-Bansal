311edf969e67779af7787d71d4d15d00c5aae79f
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-02-19 17:46:49 +0530

    create node class

--- src/contactList/ContactList.java
+++ src/contactList/ContactList.java
@@ -1,6 +1,17 @@
 package contactList;
 
 public class ContactList {
+    private Node First;
+    private Node Last;
+    private class Node{
+        private int data;
+        private Node next;
+
+        public Node(int item){
+            this.data = item;
+        }
+    }
+
     public void addNewContact(){
 
     }
