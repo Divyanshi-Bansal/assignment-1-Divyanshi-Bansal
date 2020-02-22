c3aad0c6920336926c0a6cf4d8d109bcf10642c8
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-02-22 11:05:19 +0530

    adding Person.java

---
+++ src/person/Person.java
@@ -0,0 +1,17 @@
+package person;
+
+import java.util.ArrayList;
+
+public class Person {
+    // a field to store the first name
+    // a field to store the last name
+    // a list to store multiple contact numbers (i.e a list of numbers)
+    // a field to store the email address of the person
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<String> contacts;
+    private String email;
+
+
+}
