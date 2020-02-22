9d9154a5e9cedb508c60714bbcc5c0db7b4d8c1d
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-02-22 10:59:48 +0530

    completing template for Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,13 +1,33 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println ("Welcome to Divyanshi's Contact List App");
         System.out.println ("Press 1 to add a new contact");
         System.out.println ("Press 2 to view all contacts");
         System.out.println ("Press 3 to search for a contact");
         System.out.println ("Press 4 to delete a contact");
         System.out.println ("Press 5 to exit program");
+
+        String choice = scanner.nextLine ();
+
+        switch(choice){
+            case "1":// addContacts()
+                break;
+            case "2":// viewContacts()
+                break;
+            case "3"://search()
+                break;
+            case "4"://delete
+                break;
+            case "5"://exit
+                break;
+            default:
+                //invalid choice
+        }
 
     }
 }
