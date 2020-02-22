d31f8ab93d5f35b4b249fe16493edac13132be04
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-02-22 10:54:28 +0530

    creating Main.java

--- src/contactList/ContactList.java
+++ src/contactList/ContactList.java
@@ -1,5 +1,7 @@
 package contactList;
 
+import java.util.Scanner;
+
 public class ContactList {
     private Node First;
     private Node Last;
@@ -11,8 +13,27 @@ public class ContactList {
             this.data = item;
         }
     }
+    Scanner sc = new Scanner ( System.in );
 
     public void addNewContact(){
-
+        System.out.println ("enter your name");
+        System.out.println ("first name:"+sc.nextLine());
+        System.out.println ("last name:"+sc.nextLine ());
+        System.out.println ("contact no.:"+sc.nextLong());
+        System.out.println ("would you like to add another contact number:Y or N");
+        String C = sc.nextLine();
+        if(C=="Y"){
+            System.out.println ("contact no.:"+sc.nextLong());
+        }
+        System.out.println ("would you like to add another contact number:Y or N");
+        String C1 = sc.nextLine();
+        if(C1=="Y"){
+            System.out.println ("contact no.:"+sc.nextLong());
+        }
+        System.out.println ("would you like to add email address ? :Y or N");
+        String C2 = sc.nextLine();
+        if(C2=="Y"){
+            System.out.println ("Email Address:"+sc.nextLong());
+        }
     }
 }
